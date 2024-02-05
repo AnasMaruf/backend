61154f51422b2369d7832527b13b349301cb1bb6
AnasMaruf <anasmaruf1101@gmail.com>
2024-02-05 19:57:55 +0700

    menambahkan cors

--- src/application/web.js
+++ src/application/web.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { publicRouter } from "../route/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
+import cors from "cors";
 
 export const web = express();
+web.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 web.use(express.json());
 web.use(publicRouter);
 web.use(errorMiddleware);
